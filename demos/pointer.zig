d4b6280778639234c8683fe6a162eebcaea9c693
Michael Odden <me@michaelodden.com>
2026-09-11 08:46:10 +0200

    pointer: adds demos/pointer.zig to showcase simple pointer protocol and somewhat complex event handling (WIP)

--- demos/pointer.zig
+++ demos/pointer.zig
@@ -1,4 +1,5 @@
 /// Showcases probing for pointer devices + some more advanced event handling
+/// https://uefi.org/specs/UEFI/2.10/12_Protocols_Console_Support.html#simple-pointer-protocol
 const std = @import("std");
 const uefi = std.os.uefi;
 
@@ -55,11 +56,11 @@ pub fn main() uefi.Status {
     const screen = drawing.bitmapFromScreenbuffer(gfx_out);
     drawing.bitmapFill(screen, utils.Colors.black.argb);
 
-    var ptr_x: i32 = 10;
-    var ptr_y: i32 = 10;
+    var ptr_x: f32 = 10;
+    var ptr_y: f32 = 10;
 
     const pointer = createPointerBitmap(uefi.pool_allocator) catch unreachable;
-    drawing.blitToScreen(gfx_out, pointer, ptr_x, ptr_y);
+    // drawing.blitToScreen(gfx_out, pointer, ptr_x, ptr_y);
 
     con_out.setCursorPosition(0, 0) catch {};
     _ = con_out.outputString(utils.W("Checking for pointers")) catch unreachable;
@@ -106,28 +107,75 @@ pub fn main() uefi.Status {
 
     // Draw and animate cursor by pointer protocol
     // Main loop
+    // Draw initial
+    drawing.bltBitmapXor(screen, pointer, ptr_x, ptr_y, ptr_x + 20, ptr_y + 20);
     while (true) {
         const event = boot_services.waitForEvent(events) catch unreachable;
         // Check which type of event source
+        // Remove on previous position
+        drawing.bltBitmapXor(screen, pointer, ptr_x, ptr_y, ptr_x + 20, ptr_y + 20);
         if (event.@"1" < spp.len) {
             // Pointer event
             const state = spp[event.@"1"].getState() catch unreachable;
 
-            drawing.blitToScreen(gfx_out, pointer, ptr_x, ptr_y);
+            // TODO: Proper scale movement to pixels
+            if (state.relative_movement_x != 0 and spp[event.@"1"].mode.resolution_x != 0) {
+                ptr_x += (@as(f32, @floatFromInt(state.relative_movement_x)) / @as(f32, @floatFromInt(spp[event.@"1"].mode.resolution_x))) * 5;
+            }
 
-            ptr_x += state.relative_movement_x;
-            ptr_y += state.relative_movement_y;
-
-            drawing.blitToScreen(gfx_out, pointer, ptr_x, ptr_y);
+            if (state.relative_movement_y != 0 and spp[event.@"1"].mode.resolution_y != 0) {
+                ptr_y += (@as(f32, @floatFromInt(state.relative_movement_y)) / @as(f32, @floatFromInt(spp[event.@"1"].mode.resolution_y))) * 5;
+            }
 
             // events[event.@"1"]
             con_out.setCursorPosition(0, 5) catch {};
             _ = con_out.outputString(utils.W("pointer\r\n      ")) catch unreachable;
         } else {
             // ConIn event
+            const key = uefi.system_table.con_in.?.readKeyStroke() catch unreachable;
+            // https://uefi.org/specs/UEFI/2.11/Apx_B_Console.html
+            switch (key.scan_code) {
+                // escape
+                0x17 => break,
+                // up
+                0x01 => {
+                    ptr_y -= 5;
+                },
+                // down
+                0x02 => {
+                    ptr_y += 5;
+                },
+                // right
+                0x03 => {
+                    ptr_x += 5;
+                },
+                // left
+                0x04 => {
+                    ptr_x -= 5;
+                },
+                else => {},
+            }
+
+            // uefi.system_table.con_in.?.wait_for_key
+
             con_out.setCursorPosition(0, 5) catch {};
             _ = con_out.outputString(utils.W("     \r\nconin  \r\n")) catch unreachable;
         }
+
+        _ = utils.renderString(
+            screen,
+            100,
+            100,
+            utils.Colors.black,
+            utils.Colors.white,
+            32,
+            std.fmt.bufPrint(&scratch8, "pos: {}, {}", .{ ptr_x, ptr_y }) catch "ERR",
+        );
+
+        ptr_x = std.math.clamp(ptr_x, 0, screen.width - 20);
+        ptr_y = std.math.clamp(ptr_y, 0, screen.height - 20);
+        // Write on new
+        drawing.bltBitmapXor(screen, pointer, ptr_x, ptr_y, ptr_x + 20, ptr_y + 20);
     }
 
     utils.hangForKey(13);
