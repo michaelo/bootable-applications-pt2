0f3d609f796400a3cacf1aa53bad9f410867cd91
Michael Odden <me@michaelodden.com>
2026-09-17 22:44:00 +0200

    make-img: increases img to 16MB

--- make-img.sh
+++ make-img.sh
@@ -13,8 +13,8 @@ log Checking for mformat and mmd, if found: create $FLASHFILE
 
 if mformat --version > /dev/null && mmd --version > /dev/null ; then
     log mformat and mmd found, creating $FLASHFILE
-    dd if=/dev/zero of=$FLASHFILE bs=1k count=1440
-    mformat -i $FLASHFILE -f 1440 ::
+    dd if=/dev/zero of=$FLASHFILE bs=1k count=16384
+    mformat -i $FLASHFILE -h 64 -t 16 -n 32  ::
     mmd -i $FLASHFILE ::/EFI
     mmd -i $FLASHFILE ::/EFI/BOOT
     mcopy -i $FLASHFILE $FOLDER/EFI/BOOT/BOOTX64.EFI ::/EFI/BOOT
