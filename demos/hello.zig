6a1c142bbcc9e5d69ae34aa7335f0af4658c51c9
Michael Odden <me@michaelodden.com>
2026-09-18 02:57:36 +0200

    demos/hello.zig: Made self contained and explicit

--- demos/hello.zig
+++ demos/hello.zig
@@ -2,24 +2,20 @@
 //! Att! Not all UEFI-realizations renders console output to the screen. qemu and several tested PCs do, whereas MacBook Pros do not.
 //! https://uefi.org/specs/UEFI/2.10/12_Protocols_Console_Support.html#simple-text-output-protocol
 const std = @import("std");
-const uefi = std.os.uefi;
 
 const utils = @import("lib/utils.zig");
 
-pub fn main() uefi.Status {
+pub fn main() std.os.uefi.Status {
     // system_table.con_out is a convenience-reference to a EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL instance (if found)
     // Manual lookup will look something like: (error handling be damned)
-    const con_out = uefi.system_table.boot_services.?
-        .locateProtocol(uefi.protocol.SimpleTextOutput, null) catch null orelse unreachable;
-    // const con_out = uefi.system_table.con_out.?;
+    const con_out = std.os.uefi.system_table.boot_services.?
+        .locateProtocol(std.os.uefi.protocol.SimpleTextOutput, null) catch null orelse unreachable;
+    // const con_out = std.os.uefi.system_table.con_out.?;
     con_out.reset(false) catch {};
     con_out.setCursorPosition(2, 2) catch {};
 
     // UEFI works with wide (UTF-16) strings, (stdlib has convenient functions to convert UTF8->UTF16)
     _ = con_out.outputString(&[_:0]u16{ 'H', 'e', 'l', 'l', 'o', 0 }) catch {};
-
-    // Ignore this for now
-    utils.hangForKey(13);
 
     return .success;
 }
