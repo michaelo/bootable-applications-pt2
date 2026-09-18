e1a4a2fb7f41542f73add22f55227c92b50d4476
Michael Odden <me@michaelodden.com>
2026-09-18 14:36:33 +0200

    adjustments before talk

--- demos/hello.zig
+++ demos/hello.zig
@@ -6,16 +6,21 @@ const std = @import("std");
 const utils = @import("lib/utils.zig");
 
 pub fn main() std.os.uefi.Status {
-    // system_table.con_out is a convenience-reference to a EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL instance (if found)
+    // system_table.con_out is a convenience-reference to a
+    // EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL instance (if found)
     // Manual lookup will look something like: (error handling be damned)
     const con_out = std.os.uefi.system_table.boot_services.?
         .locateProtocol(std.os.uefi.protocol.SimpleTextOutput, null) catch null orelse unreachable;
-    // const con_out = std.os.uefi.system_table.con_out.?;
+
     con_out.reset(false) catch {};
     con_out.setCursorPosition(2, 2) catch {};
 
-    // UEFI works with wide (UTF-16) strings, (stdlib has convenient functions to convert UTF8->UTF16)
+    // UEFI works with wide (UTF-16) strings, (stdlib has convenient functions to
+    // convert UTF8->UTF16)
     _ = con_out.outputString(&[_:0]u16{ 'H', 'e', 'l', 'l', 'o', 0 }) catch {};
+
+    // Ignore this for now
+    utils.hangForKey(13);
 
     return .success;
 }
